e56cd543a7f783f57639559d537a1e96b4798dc4
Harish Palani <harishpalani@berkeley.edu>
2019-01-02 18:44:15 -0800

    DB & Devise gem setup complete.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :shots
+  devise_for :users, controller: { registrations: 'registrations' }
+  root 'home#index'
 end
