39ca54f04264d3a4d0be9cbac563681a7530267f
Harish Palani <harishpalani@berkeley.edu>
2019-01-08 12:36:21 -0800

    Submission page refreshed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_023055) do
+ActiveRecord::Schema.define(version: 2019_01_08_202903) do
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_01_03_023055) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_shot"
   end
 
   create_table "users", force: :cascade do |t|
